a2d494891f0622a4f6e9219bdc633c4529186a70
glaszig <glaszig@gmail.com>
2013-03-15 19:48:10 +0100

    initial commit

---
+++ lib/compass/fontcustom.rb
@@ -0,0 +1,7 @@
+require "compass/fontcustom/version"
+
+module Compass
+  module Fontcustom
+    # Your code goes here...
+  end
+end
---
+++ lib/compass/fontcustom/version.rb
@@ -0,0 +1,5 @@
+module Compass
+  module Fontcustom
+    VERSION = "0.0.1"
+  end
+end
