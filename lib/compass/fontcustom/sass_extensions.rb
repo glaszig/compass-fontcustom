f1fd7914fef7b086c6ba27d27e9252fcf6bae248
glaszig <glaszig@gmail.com>
2013-03-27 00:25:04 +0100

    fixing white spaces

--- lib/compass/fontcustom/sass_extensions.rb
+++ lib/compass/fontcustom/sass_extensions.rb
@@ -1,19 +1,19 @@
 module Compass
   module Fontcustom
-  	module SassExtensions
+    module SassExtensions
 
-  		module Functions
+      module Functions
 
-  			def letter(index)
-  				idx = (61696+index.value).to_s(16)
-  				css = %Q[&:before { content: "\\#{idx}"; }]
-  				Sass::Script::String.new %Q["\\#{idx}"]
-  			end
-  			Sass::Script::Functions.declare :letter, [:index]
+        def letter(index)
+          idx = (61696+index.value).to_s(16)
+          css = %Q[&:before { content: "\\#{idx}"; }]
+          Sass::Script::String.new %Q["\\#{idx}"]
+        end
+        Sass::Script::Functions.declare :letter, [:index]
 
-  		end
+      end
 
-  	end
+    end
   end
 end
 
