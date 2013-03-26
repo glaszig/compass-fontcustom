7df8c38234b1957574deaa1a0cc91ddca19db9c1
glaszig <glaszig@gmail.com>
2013-03-26 22:23:42 +0100

    adding pry to test helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,7 @@
 require 'test/unit'
 require 'compass/fontcustom'
 require 'helpers/test_case'
+require 'pry'
 
 class Test::Unit::TestCase
 	extend Compass::Fontcustom::TestCaseHelper::ClassMethods
