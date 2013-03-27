3bbf3ca62f6435790db1f3caa762deb1c52272c4
glaszig <glaszig@gmail.com>
2013-03-27 23:39:45 +0100

    refactored css templates, registering ourselves as a compass framework

--- lib/compass/fontcustom.rb
+++ lib/compass/fontcustom.rb
@@ -6,10 +6,13 @@ require "compass/fontcustom/font_importer"
 
 module Compass
   module Fontcustom
-    Sass.load_paths << FontImporter.new
+    base_directory  = File.expand_path('../../../', __FILE__)
+    Compass::Frameworks.register('fontcustom', :path => base_directory)
 
     Compass::Configuration.add_configuration_property(:fontcustom_hash, "enables/disables fontcustom file name hashing") do
       true
     end
+
+    Sass.load_paths << FontImporter.new
   end
 end
